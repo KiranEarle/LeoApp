3a02b622ec18d40e683ba7c2069bdd3fc99a0018
KiranEarle <K.Earle@ibsv.com>
2016-11-14 15:57:59 +0000

    database

--- LeoApp/app.js
+++ LeoApp/app.js
@@ -22,9 +22,8 @@ var setUpPassport = require("./setuppassport");
 
 var app = express();
 
-mongoose.connect("mongodb://localhost:27017/Lion", function(){
-	//mongodb://Kiran:<dbpassword>@ds019471.mlab.com:19471/lion
-	//
+mongoose.connect("mongodb://Kiran:Myleskusume1@ds019471.mlab.com:19471/lion", function(){
+	//mongodb://localhost:27017/Lion
 	console.log('Connected to Mongo server')
 });
 setUpPassport();
